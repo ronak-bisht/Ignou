924cc4cef1a0fe176c807d53053cc8a727e6bb5a
ronak-bisht <107303590+ronak-bisht@users.noreply.github.com>
2023-04-24 23:24:47 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import colors from "colors";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://root:babyboss@cluster0.5mes4.mongodb.net/e-shopping?retryWrites=true&w=majority");
+    const conn = await mongoose.connect("");
     console.log(
       `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
     );
